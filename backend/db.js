4cdc3cbe46868960922943284fc9b8f0acd008a8
TheAdityaJain <jain.aditya1020@gmail.com>
2024-09-14 15:17:22 +0530

    db connection

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.12",
+      {}
+    );
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+  }
+};
+
+module.exports = connectToMongo;
